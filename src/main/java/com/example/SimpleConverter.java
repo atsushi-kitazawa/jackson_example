51ca7d1cb2adbebe275b3a8d67c42b5278953af7
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-07-10 00:52:01 +0900

    rename Converter to SimpleConverter

--- src/main/java/com/example/SimpleConverter.java
+++ src/main/java/com/example/SimpleConverter.java
@@ -4,7 +4,7 @@ import com.example.entity.Entity;
 import com.example.entity.EntityFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Converter {
+public class SimpleConverter {
 
 	private void JsonToClass(String json) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
@@ -19,7 +19,7 @@ public class Converter {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Converter c = new Converter();
+		SimpleConverter c = new SimpleConverter();
 		c.ClassToJson(EntityFactory.create());
 
 		String json = "{\"id\":1,\"str\":\"abc\",\"list\":[\"AAA\",\"BBB\",\"CCC\"],\"map\":{\"key2\":\"value2\",\"key1\":\"value1\"}}";
