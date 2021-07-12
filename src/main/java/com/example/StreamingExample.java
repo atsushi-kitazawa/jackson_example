9490d4cc6c59c6e881b0e9f047ebe75e47ad4107
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-07-13 00:00:52 +0900

    implement jsonToClass method in StreamingExample class.

--- src/main/java/com/example/StreamingExample.java
+++ src/main/java/com/example/StreamingExample.java
@@ -3,8 +3,14 @@ package com.example;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.example.entity.Entity;
+import com.example.entity.SubEntity;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
@@ -15,53 +21,74 @@ public class StreamingExample {
 		JsonFactory factory = new JsonFactory();
 		JsonParser parser = factory.createParser(json);
 
+		Entity entity = new Entity();
+
 		while (parser.nextToken() != JsonToken.END_OBJECT) {
 			String fieldName = parser.getCurrentName();
 			if ("id".equals(fieldName)) {
 				parser.nextToken();
 				print(fieldName, parser.getText());
+				entity.setId(Integer.parseInt(parser.getText()));
 			}
 
 			if ("str".equals(fieldName)) {
 				parser.nextToken();
 				print(fieldName, parser.getText());
+				entity.setStr(parser.getText());
 			}
 
 			if ("list".equals(fieldName)) {
 				parser.nextToken();
+				List<String> list = new ArrayList<>();
 				while (parser.nextToken() != JsonToken.END_ARRAY) {
 					print(fieldName, parser.getText());
+					list.add(parser.getText());
 				}
+				entity.setList(list);
 			}
 
 			if ("map".equals(fieldName)) {
 				parser.nextToken();
+				Map<String, String> map = new HashMap<>();
 				while (parser.nextToken() != JsonToken.END_OBJECT) {
-					print(fieldName, parser.getText());
+					String key = parser.getText();
+					parser.nextToken();
+					String val = parser.getText();
+					print(fieldName, key);
+					print(fieldName, val);
+					map.put(key, val);
 				}
+				entity.setMap(map);
 			}
 
 			if ("subList".equals(fieldName)) {
 				parser.nextToken();
+				List<SubEntity> list = new ArrayList<>();
 				while (parser.nextToken() != JsonToken.END_ARRAY) {
+					SubEntity subEntity = new SubEntity();
 					while (parser.nextToken() != JsonToken.END_OBJECT) {
 						String subStrField = parser.getCurrentName();
 						if ("subStr".equals(subStrField)) {
 							parser.nextToken();
 							print(fieldName, parser.getText());
+							subEntity.setSubStr(parser.getText());
 						}
 					}
+					list.add(subEntity);
 				}
+				entity.setSubList(list);
 			}
 		}
+
+		System.out.println(entity);
 
 		parser.close();
 	}
 
 	private void print(String field, String value) {
-		System.out.println(field + ":" + value);
+		//System.out.println(field + ":" + value);
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		StreamingExample se = new StreamingExample();
 		String json = Files
